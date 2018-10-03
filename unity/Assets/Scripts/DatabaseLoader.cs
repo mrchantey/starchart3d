678de243b56ba4ea2cc7f62f933c6c8c6e385172
Peter Hayman <peteyhayman@gmail.com>
2018-10-03 15:18:09 +1000

    lesson 2 commit

---
+++ unity/Assets/Scripts/DatabaseLoader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DatabaseLoader : ScriptableObject
+{
+
+    public abstract void LoadDatabase();
+
+}
---
+++ unity/Assets/Scripts/StarDatabaseLoader.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+
+
+
+[CreateAssetMenu(menuName = "StarChart/Databases/Star")]
+public class StarDatabaseLoader : DatabaseLoader
+{
+
+    public string databasePath;
+    public StarInfo[] stars;
+
+
+    public override void LoadDatabase()
+    {
+        string[] lines = IOUtility.OpenLines(databasePath);
+        foreach (var line in lines)
+        {
+            Debug.Log(line);
+        }
+    }
+
+
+
+}
